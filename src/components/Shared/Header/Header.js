f7c65617f9b5e0c0eac8510f08563ba1c0f45f3e
SADEK HOSSAIN <shadeks16@gmail.com>
2022-04-16 17:52:15 +0600

    primary style part added

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -3,8 +3,18 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from '../../../images/logo2.png';
 import cart from '../../../images/icons/shopping-cart.png';
 import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { signOut } from "firebase/auth";
+import './Header.css';
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () =>{
+    signOut(auth);
+  };
+
   return (
     <>
       <Navbar collapseOnSelect expand="lg" sticky="top" variant="light">
@@ -14,8 +24,8 @@ const Header = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="cart"><img style={{height:'25px'}} className="w-100" src={cart} alt="Shopping Cart Logo" /></Nav.Link>
-              <Nav.Link as={Link} to="signin">Sign In</Nav.Link>
-              <Nav.Link as={Link} to="signup">Sign Up</Nav.Link>
+              {user ? <button className="btn btn-link text-decoration-none text-dark nav-style" onClick={handleSignOut}>Sign Out</button> :<Nav.Link className="nav-style text-dark" as={Link} to="signin">Sign In</Nav.Link>}
+              <Nav.Link className="nav-style text-dark" as={Link} to="signup">Sign Up</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
